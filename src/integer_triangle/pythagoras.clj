93184db501f505f6f7647d8524ed878177449931
MISUMI Masaru <deltam@users.noreply.github.com>
2022-02-08 22:50:33 +0900

    Update pythagoras triangle

--- src/integer_triangle/pythagoras.clj
+++ src/integer_triangle/pythagoras.clj
@@ -11,8 +11,30 @@
                    (filter not=odd)))
 
 (defn mn->triangle [[m n]]
-  (list (- (* m m) (* n n))
-        (* 2 m n)
-        (+ (* m m) (* n n))))
+  (let [[a b c] (sort > [(- (* m m) (* n n))
+                         (* 2 m n)
+                         (+ (* m m) (* n n))])]
+    (c/tri [a b c] (* b c 1/2))))
 
 (def pythagorean-triangles (map mn->triangle mn-pairs))
+
+
+
+
+(comment
+
+(->> py/pythagorean-triangles
+     (map (fn [p]
+            (let [[a b c] (:side p)]
+              (assoc p
+                     :n (int (Math/floor (/ (* 2 Math/PI) (q/acos (cos {:side [c b a]})))))
+                     :mid (/ (:area %) (sq a))))))
+     (filter #(< 3.05 (* (:n %) (:mid %)) Math/PI))
+     (take 3)
+     )
+
+;=> ({:side [113 112 15], :area 840N, :n 47, :mid 840/12769}
+;=>  {:side [145 144 17], :area 1224N, :n 53, :mid 1224/21025}
+;=>  {:side [181 180 19], :area 1710N, :n 59, :mid 1710/32761})
+
+)
