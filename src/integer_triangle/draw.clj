03317ba41995536b39efa11eaa584f1a8fbb2842
MISUMI Masaru <deltam@users.noreply.github.com>
2022-02-08 22:52:47 +0900

    Draw triangle and trapezoid

--- src/integer_triangle/draw.clj
+++ src/integer_triangle/draw.clj
@@ -71,49 +71,85 @@
     0.0
     (q/acos (hr/trapezoid-leg-cos base-tp))))
 
-(defn draw-outer [ot angle]
-  (q/with-rotation [angle]
-;    (draw-triangle ot)
-    ;(draw-diameter ot)
-;    (q/with-fill [(q/color 200)])
-;    (draw-triangle (cr/mirror ot))
-    (let [tp (hr/triangle->trapezoid ot)]
-      (draw-trapezoid tp)
-      (let [tp2 (hr/trapezoid->trapezoid tp)
-            ang (trapezoid-angle tp tp2)]
-        (q/with-rotation [ang]
-          (draw-trapezoid tp2))
-        (q/with-rotation [(- ang)]
-          (draw-trapezoid tp2))))
+(defn draw-outer [ot]
+;  (draw-triangle ot)
+;  (draw-diameter ot)
+                                        ;    (q/with-fill [(q/color 200)])
+ ; (draw-triangle (cr/mirror ot))
+;  (draw-diagonal ot)
+;  (draw-trapezoid (hr/triangle->trapezoid ot))
+
+  (let [tp (hr/triangle->trapezoid ot)]
+    (draw-trapezoid tp)
+    (let [tp2 (hr/trapezoid->trapezoid tp)
+          ang (trapezoid-angle tp tp2)]
+      (q/with-rotation [ang]
+        (draw-trapezoid tp2))
+      (q/with-rotation [(- ang)]
+        (draw-trapezoid tp2))))
 
                                         ;    (let [tp1 (hr/triangle->trapezoid ot)])
 
-;    (doall (take 4 (iterate draw-trapezoid-triangle [(cr/mirror ot) 0.0])))
+;  (doall (take 6 (iterate draw-trapezoid-triangle [(cr/mirror ot) 0.0])))
+;  (doall (take 6 (iterate draw-trapezoid-triangle [ot 0.0])))
 
-;    (draw-diagonal ot)
-                                        ;  (draw-head ot)
-    ))
+
+  (draw-head ot)
+  )
 
 (defn draw-all [t]
   (let [r (cr/outer-radius t)]
     (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
       (q/ellipse 0 0 (* r 2) (* r 2))
- ;     (q/with-fill [120])
-      (draw-triangle t)
-      (q/no-fill)
 
-      (draw-outer (hr/outer-triangle t) (q/radians 180))
+;     (draw-triangle t)
 
-      (draw-outer (hr/outer-triangle (cr/rotate t))
-                  (- (q/acos (cr/cos (cr/rotate (cr/rotate t))))))
 
-      (draw-outer (hr/outer-triangle (cr/rotate (cr/rotate t)))
-                  (q/acos (cr/cos (cr/rotate t))))
+      (q/with-rotation [(q/radians 180)]
+        (draw-outer (hr/outer-triangle t)))
+
+      (q/with-rotation [(- (q/acos (cr/cos (cr/rotate (cr/rotate t)))))]
+        (draw-outer (hr/outer-triangle (cr/rotate t))))
+
+      (q/with-rotation [(q/acos (cr/cos (cr/rotate t)))]
+        (draw-outer (hr/outer-triangle (cr/rotate (cr/rotate t)))))
       )))
 
 (defn resize-with-outer-radius [t d]
   (let [r (cr/outer-radius t)]
     (cr/scalling-triangle t (/ d r))))
+
+(defn draw-triangle-raw [t]
+  (->> t
+   (cr/points)
+   (flatten)
+   (apply q/triangle)))
+
+(defn draw-pythagoras-triangle [pt scl]
+  (let [[a _ _] (:side pt)
+        st (cr/scalling-triangle pt (/ scl a))
+        [r _ _] (:side st)
+        rad (q/acos (cr/cos (cr/rotate st)))
+        n (Math/floor (/ (* 2 Math/PI) rad))
+        [a b c] (:side st)
+        rt (cr/rotate (cr/rotate st))
+        lt (cr/tri [a c b] (:area st))]
+    (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
+      (q/ellipse 0 0 (* r 2) (* r 2))
+
+;      (q/debug n)
+      (q/fill (q/color 200))
+ ;     (q/stroke (q/color 200))
+      (loop [ts (take n (cycle [rt lt]))]
+        (when-let [t (first ts)]
+          (q/rotate rad)
+          (draw-triangle-raw t)
+          (recur (rest ts)))
+        )
+
+
+      ))
+  )
 
 (defn draw []
   (q/background 255)
@@ -124,7 +160,10 @@
         t4 (cr/tri [29 25 6] 60)
         t5 {:side [15N 39/4 33/4], :area 297/8}
         t6 (cr/tri [21 20 13] 126)
+        pt1 (cr/tri [113 112 15] 840)
+        pt2  {:side [3293 3268 405], :area 661770N}
         ]
+;    (draw-pythagoras-triangle pt1 350)
     (draw-all (resize-with-outer-radius t1 350))
     ))
 
